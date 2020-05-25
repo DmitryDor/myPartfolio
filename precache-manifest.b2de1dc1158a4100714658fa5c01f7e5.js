c64e1de780a7ac971d75e5f9dd4c1ef7825b9147
DmitryDor <dmitrydoroshenko80@gmail.com>
2020-05-25 13:48:30 +0300

    Updates

---
+++ precache-manifest.b2de1dc1158a4100714658fa5c01f7e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4d8bed73fddebb86e3818f077f60a73",
+    "url": "/myPartfolio/index.html"
+  },
+  {
+    "revision": "f6755ec40694c9fa45c6",
+    "url": "/myPartfolio/static/css/main.553365f1.chunk.css"
+  },
+  {
+    "revision": "579cf900bfac4cd47f18",
+    "url": "/myPartfolio/static/js/2.06383b9f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myPartfolio/static/js/2.06383b9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6755ec40694c9fa45c6",
+    "url": "/myPartfolio/static/js/main.82e2fd03.chunk.js"
+  },
+  {
+    "revision": "20207b4760c46f0c7ab6",
+    "url": "/myPartfolio/static/js/runtime-main.eed82111.js"
+  }
+]);
