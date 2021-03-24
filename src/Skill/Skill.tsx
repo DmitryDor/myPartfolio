442bbc41e9e403e07792819554625da9ca95fba7
Your NameDmitryDor <you@example.com>
2021-03-24 17:43:54 +0300

    2.1(step3 - block 'Skills')

--- src/Skill/Skill.tsx
+++ src/Skill/Skill.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import style from './Skill.module.css';
+import style from './Skill.module.scss';
 
 type PropsType = {
     title: string,
     description: string
+    icon: any
 }
 
 function Skill(props: PropsType) {
     return (
         <div className={style.skill}>
-
-            <div className={style.icon}></div>
-            <h3>{props.title}</h3>
-            <span className={style.description}>{props.description}</span>
+            <div className={style.icon}>{props.icon}</div>
+            <div className={style.skillInfo}>
+                <h3 className={style.skillTitle}>{props.title}</h3>
+                <div className={style.description}>{props.description}</div>
+            </div>
         </div>
     );
 }
--- src/skills/Skills.tsx
+++ src/skills/Skills.tsx
@@ -1,22 +1,34 @@
 import React from 'react';
-import style from './Skills.module.css';
+import style from './Skills.module.scss';
 import styleContainer from './../common/styles/Container.module.css'
-import Skill from "../Skill/Skill";
-import {Title} from "../common/components/title/title";
+import Skill from '../Skill/Skill';
+import {Title} from '../common/components/title/title';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faHtml5, faJs, faReact} from '@fortawesome/free-brands-svg-icons';
+import {faEnvelope, faFileCode} from '@fortawesome/free-solid-svg-icons';
+
 
 function Skills() {
     return (
-        <div className={style.skillsblock}>
-            <div className= {`${styleContainer.container} ${style.skillsContainer}`} >
-               {/*<h2 className={style.title}> Skills</h2>*/}
-               <Title title = 'Skills'/>
+        <div className={style.skillsBlock}>
+            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+                <Title title='Skills'/>
                 <div className={style.skills}>
-                    <Skill title={"JS"} description={"Description of knowledge in JS"}/>
-                    <Skill title={"HTML"} description={"Description of knowledge in HTML "}/>
-                    <Skill title={"CSS"} description={"Description of knowledge in CSS "}/>
+                    <Skill icon={<FontAwesomeIcon icon={faJs} size='6x'/>} title={'JAVASCRIPT'}
+                           description={'Description of knowledge in JS sifgiusiufhishfh  fsdfdff'}/>
+                    <Skill icon={<FontAwesomeIcon icon={faHtml5} size='6x'/>} title={'HTML'}
+                           description={'Description of knowledge in HTML '}/>
+                    <Skill icon={<FontAwesomeIcon icon={faFileCode} size='6x'/>} title={'CSS '}
+                           description={'Description of knowledge in CSS Description of knowledge in JS ' +
+                           'sifgiusiufhishfh ' + ' fsdfdff Description of knowledge in JS sifgiusiufhishfh  ' +
+                           'fsdfdffDescription of knowledge in' +
+                           ' JS sifgiusiufhishfh  fsdfdff'}/>
+                    <Skill icon={<FontAwesomeIcon icon={faReact} size='6x'/>} title={'REACT'}
+                           description={'Description of knowledge in CSS '}/>
                 </div>
             </div>
         </div>
     );
 }
+
 export default Skills;
