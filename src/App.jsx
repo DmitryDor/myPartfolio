4332bc45779a56fc17d7870358fd065d055c81fd
Your NameDmitryDor <you@example.com>
2021-03-28 09:33:12 +0300

    3.1(step7 - before switching to js)

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+import {Contact} from './a6-contact/Contact';
+import {Header} from './a1-header/Header';
+import {Skills} from './a3-skills/Skills';
+import { MyProjects } from './a4-projects/MyProjects';
+import { Remote } from './a5-remote/Remote';
+import {Footer} from "./a7-footer/Footer";
+import Main from './a2-main/Main';
+
+
+export function App() {
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Skills/>
+            <MyProjects/>
+            <Remote/>
+            <Contact/>
+            <Footer/>
+        </div>
+    );
+}
+
+
